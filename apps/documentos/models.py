8de63dba2f74c6924e247842d6510e0d7720383d
Thiago Andrade <thgas95@gmail.com>
2019-11-17 22:15:08 -0300

    Criando os relacionametos de banco de dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionarios
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertence = models.ForeignKey(Funcionarios, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionarios
 
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    funcionario = models.ForeignKey(Funcionarios, on_delete=models.PROTECT)
 
 
     def __str__(self):
